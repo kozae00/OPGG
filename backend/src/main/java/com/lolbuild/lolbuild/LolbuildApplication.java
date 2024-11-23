c05b4079185373ec92da941a56df3cf3ff1d0e9f
kozae00 <168618934+Kozae0@users.noreply.github.com>
2024-11-23 10:15:58 +0900

    Initial upload of local files

---
+++ backend/src/main/java/com/lolbuild/lolbuild/LolbuildApplication.java
@@ -0,0 +1,13 @@
+package com.lolbuild.lolbuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolbuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LolbuildApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/lolbuild/lolbuild/LolbuildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lolbuild.lolbuild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LolbuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
